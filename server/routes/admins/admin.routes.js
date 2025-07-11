714076fdf24960139bca329e307f6ab7dc4a937e
Abdul Waqas <www.waqas7172@gmail.com>
2025-07-11 12:49:12 +0500

    add get users api for admin

--- server/routes/admins/admin.routes.js
+++ server/routes/admins/admin.routes.js
@@ -1,8 +1,13 @@
 let express = require('express');
 const createAdmin = require('../../controllers/admins/create-admin');
 const upload = require('../../middlewares/upload.md');
+const getCompanyUsers = require('../../controllers/admins/get-users');
+const authorizeRoles = require('../../middlewares/authorize-roles.md');
+const verifyToken = require('../../middlewares/token-verify.md');
 let router = express.Router();
 
-router.post('/create/admin/:id',upload.single('profileImage'),createAdmin)
+router.post('/create/admin/:id',upload.single('profileImage'),createAdmin);
+router.get('/admin/get-users/:id',verifyToken,authorizeRoles("admin"),getCompanyUsers);
+// router.get('/admin/get-users/:id',getCompanyUsers); for testing
 
 module.exports = router;
